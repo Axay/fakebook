f63bf076e5366ecb9a95b84943f47ce24d2120ef
Akshay Shailesh <akshay.brandradiator@gmail.com>
2025-05-20 08:34:39 +0530

    Signup form and password input UI comp

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from "next";
 import signupImage from "@/assets/signup-image.jpg";
 import Image from "next/image";
+import Link from "next/link";
+import SignUpForm from "./SignUpForm";
 
 export const metadata: Metadata = {
   title: "Sign Up",
@@ -11,7 +13,16 @@ export default function Page() {
     <main className="flex h-screen items-center justify-center p-5">
       <div className="flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-2xl bg-card shadow-2xl">
         <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
-          login form
+          <div className="space-y-10 text-center">
+            <h1 className="text-3xl font-bold">Sign up to fakebook</h1>
+            <p>A place where <span className="italic">you</span> can find friend</p>
+          </div>
+          <div className="space-y-5">
+            <SignUpForm />
+            <Link href="/login" className="block text-center hover:underline">
+                Already have an account? Log in
+            </Link>
+          </div>
         </div>
         <Image
           src={signupImage}
